7430124ee243edff4eaf2b1ab6f626818391595d
Your Name <you@example.com>
2024-10-25 23:59:23 +0300

    Updated project with new features

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 // src/Login.js
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
-import '../styles/Login.css';  // Adjust path to point to styles folder
+import '../styles/Styles.css';  // Adjust path to point to styles folder
 
 
 const Login = () => {
@@ -21,50 +21,59 @@ const Login = () => {
         <>
 
             {/* Login form */}
-            <div className="login-container">
-                {/* Header with logo and text */}
-                <div class="logo-text-container ">
-                    <img src="/logoo.jpg" alt="Logo" class="logo-image" />
-                    <span class="header-text">Academic Training Center</span>
-                </div>
-                <h2>Login</h2>
-                <form onSubmit={handleLogin}>
-                    <div className="form-group">
-                        <label>
-                            Username:
-                            <input
-                                type="text"
-                                name="username"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                placeholder="Enter your username"
-                                required
-                            />
-                        </label>
-                        <label>
-                            Password:
-                            <input
-                                type="password"
-                                name="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                placeholder="Enter your password"
-                                required
-                            />
-                        </label>
-                    </div>
-                    <button type="submit">Login</button>
+            <div class="allContent">
+                <div class="fancyBG"></div>
+                <div class="contentDiv">
+                    <div className="login-container">
+                        {/* Header with logo and text */}
+                        <div class="logo-text-container ">
+                            <img src="/logoo.jpg" alt="Logo" class="logo-image" />
+                            <span class="header-text">Academic Training Center</span>
+                        </div>
+                        <h2 class="loginTitle">Login</h2>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-group">
+                                <label class="loginLabel">
+                                    Username:
+                                </label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    class="loginInput"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    placeholder="Enter your username"
+                                    required
+                                />
 
-                    {/* Additional links */}
-                    <div className="extra-buttons">
-                        <Link to="/forgot-password" className="forgot-password">
-                            Forgot Password?
-                        </Link>
-                        <Link to="/signup" className="sign-up">
-                            Sign Up
-                        </Link>
+                                <label class="loginLabel">
+                                    Password:
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    class="loginInput"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Enter your password"
+                                    required
+                                />
+
+                            </div>
+                            <button type="submit" class="loginButton">Login</button>
+
+                            {/* Additional links */}
+                            <div className="extra-buttons">
+                                <Link to="/forgot-password" className="forgot-password">
+                                    Forgot Password?
+                                </Link>
+                                <Link to="/signup" className="sign-up">
+                                    Sign Up
+                                </Link>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </>
     );
